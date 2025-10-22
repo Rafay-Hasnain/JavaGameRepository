47bb6a29232bca5a416d205e415a893b38429875
Rafay-Hasnain <rafayhasnain242@gmail.com>
2025-10-22 21:03:52 +0500

    Final Commit

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -4,17 +4,18 @@ import java.util.Scanner; //The Scanner class which is inside the java.util libr
 //Non Static (Variable or Method) = Belongs to an object (instance) of the class when we create it using new. 
 public class MazeRunner {
 
-    public String MazeArray[][]; // MazeArray is a global class level variable. We can only declare global class
-                                 // variables outside the main method.
-    public int PlayerRow = 1;
-    public int PlayerColumn = 1;
+    String MazeArray[][]; // MazeArray is a global class level variable. We can only declare global class
+                          // variables outside the main method.
+    int PlayerRow = 1; // We only need public if other classes need to access these variables, no need
+                       // if they are in the same class. Same goes for methods.
+    int PlayerColumn = 1;
 
-    public int stepsTaken = 0;
+    int stepsTaken = 0;
 
     long startTime; // Using long ensures you can store large millisecond values.
     long endTime;
 
-    int highScore; // To store the high score depending upon the time taken to reach exit.
+    int bestTimeScore = 0;
 
     Scanner myObj = new Scanner(System.in); // Scanner is a class so we need to make an object first to use it. It
                                             // acts like a reader that listens to what the user types into the
@@ -122,7 +123,7 @@ public class MazeRunner {
     public String exitGame() {
 
         String message;
-        message = "Exiting the game";
+        message = "Exiting the Game";
         return message;
 
     }
@@ -131,15 +132,30 @@ public class MazeRunner {
         endTime = (System.currentTimeMillis() - startTime) / 1000; // This will convert it into seconds.
         System.out.println("The number of steps taken:" + stepsTaken);
         System.out.println("Time played:" + endTime + " seconds");
+        updateScore();
+    }
+
+    public void updateScore() { // Local variables only exist when the method is called and are lost when the
+                                // method is done.
+
+        if (bestTimeScore == 0 || endTime < bestTimeScore) {
+            bestTimeScore = (int) endTime; // (int) converts long into an integer so we need to match the variables.
+
+        }
+
+    }
+
+    public void showHighScore() {
+        System.out.println("The highest score is:" + bestTimeScore + " seconds");
     }
 
     public void showInstructions() {
         System.out.println("Instructions");
-        System.out.println("Reach the exit in the least amount of time for maximum score!");
-        System.out.println("Press 'W' to move up");
-        System.out.println("Press 'A' to move left");
-        System.out.println("Press 'S' to move down");
-        System.out.println("Press 'D' to move right");
+        System.out.println("Reach the exit in the least amount of time for maximum score");
+        System.out.println("Press 'W' to Move Up");
+        System.out.println("Press 'A' to Move Left");
+        System.out.println("Press 'S' to Move Down");
+        System.out.println("Press 'D' to Move Right");
     }
 
     public void showCredits() {
@@ -151,6 +167,9 @@ public class MazeRunner {
         // Timer:
         startTime = System.currentTimeMillis(); // System.currentTimeMillis() returns the current time in milliseconds
                                                 // stored in startTime.
+        PlayerRow = 1; // Reset these values otherwise P will be at the old position.
+        PlayerColumn = 1;
+        stepsTaken = 0;
 
         inititalizeMaze(); // We need to initialize the maze first as it needs to exist before we can make
                            // a move.
@@ -160,11 +179,11 @@ public class MazeRunner {
         while (running) {
 
             // To quit the game:
-            System.out.print("Press 'Q' to quit the game or W/A/S/D to move the player:");
+            System.out.println("Press 'Q' to quit the game or W/A/S/D to move the player:");
             String Choice = myObj.nextLine().toUpperCase();
 
             if (Choice.equals("Q")) {
-                System.out.print(exitGame());
+                System.out.println(exitGame());
                 displayResults();
                 running = false;
 
@@ -173,7 +192,7 @@ public class MazeRunner {
             else if (Choice.equals("W") || Choice.equals("A") || Choice.equals("S") || Choice.equals("D")) {
 
                 if (isValidMove(Choice).equals("Hit a wall")) {
-                    System.out.println("Hit a wall");
+                    System.out.println("Hit a Wall");
                     printMaze();
                 } else {
                     movePlayer(Choice);
@@ -181,7 +200,8 @@ public class MazeRunner {
                     System.out.println("Moved");
 
                     if (hasPlayerWon().equals("You have Won!")) {
-                        System.out.println("You have Won!");
+                        System.out.println("You Won!");
+                        updateScore();
                         displayResults();
                         running = false;
                     }
@@ -190,7 +210,7 @@ public class MazeRunner {
             }
 
             else {
-                System.out.print("Invalid Entry");
+                System.out.println("Invalid Entry");
                 printMaze();
             }
         }
@@ -201,16 +221,17 @@ public class MazeRunner {
 
         MazeRunner game = new MazeRunner(); // Object
 
-        System.out.println("Press '1' to start the game");
-        System.out.println("Press '2' to see the instructions");
-        System.out.println("Press '3' to show the credits");
-        System.out.println("Press '4' to show the high score");
-        System.out.println("Press '5' to exit");
+        System.out.println("Press '1' to start the Game");
+        System.out.println("Press '2' to see the Instructions");
+        System.out.println("Press '3' to show the Credits");
+        System.out.println("Press '4' to show the High Score");
+        System.out.println("Press '5' to Exit");
 
         boolean running = true;
         while (running) {
             // Making the main menu:
-            System.out.print("Press '1' to play game or 2/3/4/5 (5 to exit):");
+            System.out.println(
+                    "Press '1' to Play Game or (2:instructions/3:credits/4:high score/5:exit):");
             int choice = game.myObj.nextInt(); // Reads the number.
             game.myObj.nextLine(); // Reads the leftover newline (we need to erase it) so that String LetterChoice
             // = myObj.nextLine(); reads the actual letter instead of the new line which
@@ -218,19 +239,19 @@ public class MazeRunner {
 
             if (choice == 1) {
                 game.playGame();
-                ;
+
             } else if (choice == 2) {
                 game.showInstructions();
-                ;
+
             } else if (choice == 3) {
                 game.showCredits();
             } else if (choice == 4) {
-                System.out.print("High Score");
+                game.showHighScore();
             } else if (choice == 5) {
                 System.out.print("Exiting");
                 running = false;
             } else {
-                System.out.print("You entered the wrong value!");
+                System.out.println("You entered the wrong value!");
             }
 
         }
